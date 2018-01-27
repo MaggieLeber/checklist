430f7a4c82beb864783bb26256f2095c52a44fee
Maggie Leber <maggie@matrisync.com>
2018-01-26 19:51:45 -0500

    WIP

--- src/main/scala/checklist/Checklist.scala
+++ src/main/scala/checklist/Checklist.scala
@@ -23,6 +23,7 @@ object Checklist {
   case class Checklist(uuid:String,
                        checklistItems:List[String],
                        name:String,
+                       `type`:Int,
                        subtype:Int)
 
   case class Binder(uuid:String,
@@ -34,19 +35,40 @@ object Checklist {
 
   val itemMap = blobject.checklistItems map (t => t.uuid -> t) toMap
 
+  def formatItems(filter:Checklist=>Boolean) = {
+    for (cl <- blobject.checklists.filter(filter))
+      yield
+        div(p(cl.name),ul(for (cli <- cl.checklistItems) yield
+          if (itemMap(cli).itemType == 11)
+            p(s"${itemMap(cli).title} -- ${itemMap(cli).action}")
+            else
+            li(s"${itemMap(cli).title} : ${itemMap(cli).action}")))
+  }
+
+  def preflight(p:Checklist):Boolean = (p.subtype == 0) && (p.`type` == 0)
+  def cruise(p:Checklist):Boolean = (p.subtype == 1) && (p.`type` == 0)
+  def landing(p:Checklist):Boolean = (p.subtype == 2) && (p.`type` == 0)
+  def abnormal(p:Checklist):Boolean = p.`type` == 1
+  def emergency(p:Checklist):Boolean = p.`type` == 2
+
   def htmlChecklist =
     html(
       head(script("")),
       body(
         h1("C-177B Checklists"),
         div(
-
-          for (cl <- blobject.checklists)
-            yield div(p(cl.name+":"+cl.subtype),
-              ul(
-              for (cli <- cl.checklistItems) yield li(s"${itemMap(cli).title} : ${itemMap(cli).action} (Type ${itemMap(cli).itemType})")
-              )
-    ))))
+          h2("Normal"),
+           h3("preflight"),
+            formatItems(preflight),
+          h3("Takeoff/cruise"),
+            formatItems(cruise),
+          h3("Landing"),
+            formatItems(landing),
+          h2("Abnormal"),
+            formatItems(abnormal),
+          h2("Emergency"),
+            formatItems(emergency),
+    )))
 
   def writeHtml = {
     val pw =new java.io.PrintWriter("htmlChecklist.html")
